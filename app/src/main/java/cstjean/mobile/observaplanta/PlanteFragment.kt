4497eeb74077de5402a3907cba093293abb0355f
olivier3 <olivierbilodeau@outlook.com>
2022-10-10 13:58:13 -0400

    fix: graddle build for binding

--- app/src/main/java/cstjean/mobile/observaplanta/PlanteFragment.kt
+++ app/src/main/java/cstjean/mobile/observaplanta/PlanteFragment.kt
@@ -1,24 +1,19 @@
 package cstjean.mobile.observaplanta
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.setFragmentResultListener
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.google.zxing.BarcodeFormat
-import com.journeyapps.barcodescanner.BarcodeEncoder
 import cstjean.mobile.observaplanta.databinding.FragmentPlanteBinding
 import cstjean.mobile.observaplanta.plante.Plante
 import kotlinx.coroutines.launch
-import java.util.*
 
 class PlanteFragment : Fragment(){
     private var _binding: FragmentPlanteBinding? = null
